8bb38f6ecedf60387eae4b85f4ef05cf89720e85
IonatanDev <108241816+IonatanDev@users.noreply.github.com>
2023-06-16 13:50:50 +0000

    test

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.title('Resolucion numerica del oscilador armonico')
 
 #Definimos algunas constantes
-m = st.slider('Masa (Kg):')
-k = st.slider('Constante del resorte (N/m):')
-l_0 = st.slider('Longitud natural del resorte (m):')
+m = st.slider('Masa (Kg):', value=1.0)
+k = st.slider('Constante del resorte (N/m):', value=1.0)
+l_0 = st.slider('Longitud natural del resorte (m):', value=1.0)
 
 #Establecemos condiciones iniciales
 
