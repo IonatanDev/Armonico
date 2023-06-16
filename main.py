f9aa948ee6fcc6c6c41c174442db0a60a82d6c4c
IonatanDev <108241816+IonatanDev@users.noreply.github.com>
2023-06-16 13:49:08 +0000

    primeras pruebas

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.title('Uber pickups in NYC')
+st.title('Resolucion numerica del oscilador armonico')
+
+#Definimos algunas constantes
+m = st.slider('Masa (Kg):')
+k = st.slider('Constante del resorte (N/m):')
+l_0 = st.slider('Longitud natural del resorte (m):')
+
+#Establecemos condiciones iniciales
+
