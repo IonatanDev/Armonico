dae29e361b5b7e46a13281c1dfae98c0cb2b5104
IonatanDev <108241816+IonatanDev@users.noreply.github.com>
2023-06-16 13:59:19 +0000

    agrego las ecuaciones en latex

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ import streamlit as st
 
 st.title('Resolucion numerica del oscilador armonico')
 
+st.title('Medimos las constantes fisicas de los objetos')
 #Definimos algunas constantes
-m = st.slider('Masa (Kg):', value=1.0)
-k = st.slider('Constante del resorte (N/m):', value=1.0)
-l_0 = st.slider('Longitud natural del resorte (m):', value=1.0)
+m = st.number_input('Masa (Kg):', value=1.0)
+k = st.number_input('Constante del resorte (N/m):', value=1.0)
+l_0 = st.number_input('Longitud natural del resorte (m):', value=1.0)
 
+st.title('Determinamos las condiciones iniciales')
 #Establecemos condiciones iniciales
+x_0 = st.numeer_input('Posicion inicial (m):')
+v_0 = st.number_input('Velocidad inicial (m/s):')
+a_0 = -k*(x_0-l_0)/m
+
+st.title('Definimos el algoritmo recursivo')
+st.latex(r''' x_i = x_{i-1} + v_{i-1} \cdot \Delta t''')
+st.latex(r''' v_i = v_{i-1} + a_{i-1} \cdot \Delta t''')
+st.latex(r''' a_i = \frac{-k \cdot (l_0 - x_i)}{m}''')
+
 
